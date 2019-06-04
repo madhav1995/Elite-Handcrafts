3efd6f9a3c5d2a2153c4ed9a4a043ecfe2343886
rahulyenugula <35677781+rahulyenugula@users.noreply.github.com>
2019-06-04 12:33:49 +1000

    Add files via upload

---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Elite Handcrafts</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  </head>
+  <div class="BG">
+<div class="container"><a href="home.php"><img src="images/elite-logo.jpg" id="logo" /></a></div>
+    <div><h1 class="heading1">Elite Handcrafts</h1></div>
+
+<div class="enquiry" style="position: absolute;
+top: 10px;
+right: 10px;
+"><a href="franchise.html"><img src="images/franchise2.gif" id="enquiry" / width="250px" height="30px"></a></div>
+<br />
+
+    <div class="navbar">
+      <a href="home.php">Home</a>
+      <a href="products.php">Offers</a>
+    <a href="products.php">Products</a>
+      <a href="locations.php">Locations</a>
+      <a href="contact.php">Contact Us</a>
+      <a href="VIP.php">Become A VIP</a>
+    </div>
+
+
+    <body>
+
+
+
+    <div class="story">
+      <h3>Our Story</h3>
+      <p>
+        We are wholesale as well as retail and have a wide variety of Indian
+        products, smaller and bigger Salt lamps, Home decor, clothing etc made
+        lovingly by artisans.
+      </p>
+    </div>
+
+    <footer class="footer">
+      <h2>Elite Handcrafts</h2>
+      <p>©2019 Elite handcrafts. All Rights Reserved</p>
+      <a href="https://www.facebook.com/EliteHandcrafts02/" class="fa fa-facebook"></a>
+      <a href="https://www.instagram.com/elitehandcrafts/?hl=en" class="fa fa-instagram"></a>
+      <div class="payment-container">
+      <img src="images/paypal-footer.png" alt="Payments">
+      </div>
+    </footer>
+  </div>
+  </body>
+</html>
